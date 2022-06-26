ca0041c7b8288b4810d9f0482537f37f8495c7ca
Nxze <108155280+Nxze@users.noreply.github.com>
2022-06-26 17:40:36 +0200

    third commit

--- boh.py
+++ boh.py
@@ -7,6 +7,8 @@ def hello():
     time.sleep(1)
 
     print("So now, theoretically, I should be able to do things and commit them later.")
+    
+    print("Sorry mate, I'm here just to bother you:(")
 
 
 hello()
